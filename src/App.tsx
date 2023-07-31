606e9a3ffe59646353d28f45840bdf5e0b160cd5
adarsh-shahi <adarshshahi1009@gmail.com>
2023-07-31 18:44:50 +0530

    added side navigation menus

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SideNavigatiopn from "./components/SideNavigation";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import Chat from "./pages/Chat";
 
 function App() {
-	return <div className="bg-red-500">Hi there</div>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route
+					path="/"
+					element={
+						<ProtectedRoute>
+							<SideNavigatiopn />
+						</ProtectedRoute>
+					}
+				>
+					<Route index element={<Home />} />
+					<Route path="/chat" element={<Chat />} />
+					<Route path="/profile" element={<Profile />} />
+				</Route>
+				<Route path="/login" element={<Login />} />
+				<Route path="*" element={<h3>404. Page not found</h3>} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
