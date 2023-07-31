3e3de9108e8523cd67b2daf92ea4c3251d86f421
adarsh-shahi <adarshshahi1009@gmail.com>
2023-07-31 23:06:55 +0530

    stored user data locally, and added logout feature

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,9 +1,28 @@
-import { ReactElement } from "react";
+import { ReactElement, useContext } from "react";
+import { UserContext } from "../context/userContext";
+import { Navigate, useLocation } from "react-router-dom";
 
 export default function ProtectedRoute({
 	children,
 }: {
 	children: ReactElement;
 }) {
+	const data = useContext(UserContext);
+
+	const location = useLocation();
+	const queryParams = new URLSearchParams(location.search);
+	const userInfo = queryParams.get("jsonData");
+	if (userInfo) {
+		console.log("userInfo", userInfo)
+		localStorage.setItem("userDetails", userInfo);
+		data?.dispatch({
+			type: "ADD_LOGIN_DATA",
+			payload: JSON.parse(userInfo),
+		});
+		return <Navigate to="/"/>
+	}
+	if(data?.state.user.email === "" || data?.state.user.name === "" || data?.state.user.picture === ""){
+		return <Navigate to="/login"/>
+	}
 	return children;
 }
