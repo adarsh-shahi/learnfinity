d2a5143c0eb8fe57a4804376ad911201326970f2
adarsh-shahi <adarshshahi1009@gmail.com>
2023-07-31 22:07:21 +0530

    ouath google call

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,19 +1,39 @@
 import { FormEvent } from "react";
 
 export default function Login() {
-	const handleGoogleSignIn = () => {};
+
+
+	const handleGoogleSignIn = () => {
+		const ROOT_URL = "https://accounts.google.com/o/oauth2/v2/auth";
+		const options = {
+			redirect_uri: process.env.REACT_APP_GOOGLE_REDIRECT_URI as string,
+			client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID as string,
+			access_type: "offline",
+			response_type: "code",
+			prompt: "consent",
+			scope: [
+				"https://www.googleapis.com/auth/userinfo.profile",
+				"https://www.googleapis.com/auth/userinfo.email",
+			].join(" "),
+		};
+
+		const queryString = new URLSearchParams(options);
+		const finalSearch = `${ROOT_URL}?${queryString.toString()}`;
+		console.log(finalSearch);
+		return finalSearch;
+	};
 
 	const handleLogin = (e: FormEvent) => {
 		e.preventDefault();
 	};
 	return (
 		<div className="w-min mx-auto mt-60 flex flex-col gap-5">
-			<button
-				onClick={handleGoogleSignIn}
+			<a
+				href={handleGoogleSignIn()}
 				className="text-gray-800 flex justify-center text-2xl font-semibold border-2 border-black"
 			>
 				Continue with Google
-			</button>
+			</a>
 			<form
 				onSubmit={handleLogin}
 				className="flex flex-col bg-gray-800 p-10  gap-3 items-center"
